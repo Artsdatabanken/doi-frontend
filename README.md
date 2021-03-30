e399aa0673b994726a97d40220a4046068f4c96b
Helene Engeness Mørk <helemork@gmail.com>
2021-03-30 11:22:13 +0200

    no longer breaks when adding faulty data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # doi-frontend
 Tar inn DOI-api, presenterer dem som om det er en del av hjemmesiden
+
+## Hva består den av?
+Denne er omtrent så enkel som det er mulig å få en nettside. Den har en html, en javascript og css-stylesheets. Man trenger med andre ord ikke installere noe for å kjøre, men kan åpne index.html i nettleseren så er alt på plass. Rett og slett fordi den ikke skal gjøre så mye avansert.
+
--- doi.js
+++ doi.js
@@ -1,64 +1,88 @@
 console.log("obtain data")
 getDoiData = () => {        
-    let url = 'https://doi.test.artsdatabanken.no/api/Doi/getDoiByGuid/f6490faa-570c-fd17-070e-f635804b0159';
+
+    // Obtaining the relevant doi to look up.
+    let doi = window.location.hash;
+    doi = doi.replace("#","");
+    let url = 'https://doi.test.artsdatabanken.no/api/Doi/getDoiByGuid/'+doi;
+
+
     fetch(url)
     .then((response) => {
         return response.json()
     })
     .then((data) => {
+
+        function addData(id,content){
+            if(content!= undefined && content != null){
+                document.getElementById(id).innerHTML = content;
+            }else{
+                // To avoid entire page breaking down if one error occurs
+                console.error("no such content ", id,content);
+            }            
+        }
+
+        function appendData(id,content){
+            if(content!= undefined && content != null){
+                document.getElementById(id).appendChild(content);
+            }else{
+                // To avoid entire page breaking down if one error occurs
+                console.error("no such content ", id,content);
+            }            
+        }
+
         // Work with JSON data here
-        console.log("sucessfully fetched data")
-        document.getElementById("data.Id").innerHTML = data.data.id;
-        document.getElementById("data.Type").innerHTML = data.data.type;
+        addData("data.Id",data.data.id);
+        addData("data.Type",data.data.type);
 
         let attributes = data.data.attributes;
 
-        document.getElementById("Attributes.doi").innerHTML = attributes.doi;
-        document.getElementById("Attributes.prefix").innerHTML = attributes.prefix;
-        document.getElementById("Attributes.suffix").innerHTML = attributes.suffix;
-        document.getElementById("Attributes.identifiers").innerHTML = attributes.identifiers;
-
-
-        document.getElementById("Creators.sourcename").innerHTML = attributes.creators[0].name;
-        document.getElementById("Creators.sourcetype").innerHTML = attributes.creators[0].nameType;
+        // DOI URL 
+        addData("Attributes.doi",attributes.doi);
+        addData("Attributes.prefix",attributes.prefix);
+        addData("Attributes.suffix",attributes.suffix);
+        addData("Attributes.identifiers",attributes.identifiers);
+        addData("Creators.sourcename",attributes.creators[0].name);
+        addData("Creators.sourcetype",attributes.creators[0].nameType);
 
         // Data Creators
-        document.getElementById("Titles.type").innerHTML = attributes.titles[0].title;
-        document.getElementById("Titles.lang").innerHTML = attributes.titles[0].lang;
-        document.getElementById("publisher").innerHTML = attributes.publisher;
+        addData("Titles.type",attributes.titles[0].title);
+        addData("Titles.lang",attributes.titles[0].lang);
+        addData("publisher",attributes.publisher);
 
         // TIME
-        document.getElementById("Time.year").innerHTML = attributes.publicationYear;
+        addData("Time.year",attributes.publicationYear);
         let dates = attributes.dates;
         for(let i in dates){
-            document.getElementById("Time."+dates[i].dateType).innerHTML = dates[i].date;
+            addData("Time."+dates[i].dateType,dates[i].date);
         }
         
         // GEOLOCATION
         let geoLocations = attributes.geoLocations[0];
-        document.getElementById("GeoLocations.pointLongitude").innerHTML = geoLocations.geoLocationPoint.pointLongitude;
-        document.getElementById("GeoLocations.pointLatitude").innerHTML = geoLocations.geoLocationPoint.pointLatitude;
+        addData("geoLocationBox.eastBoundLongitude",geoLocations.geoLocationBox.eastBoundLongitude);
+        addData("geoLocationBox.northBoundLatitude",geoLocations.geoLocationBox.northBoundLatitude);
+        addData("geoLocationBox.southBoundLatitude",geoLocations.geoLocationBox.southBoundLatitude);
+        addData("geoLocationBox.westBoundLongitude",geoLocations.geoLocationBox.westBoundLongitude);
 
-        document.getElementById("geoLocationBox.eastBoundLongitude").innerHTML = geoLocations.geoLocationBox.eastBoundLongitude;
-        document.getElementById("geoLocationBox.northBoundLatitude").innerHTML = geoLocations.geoLocationBox.northBoundLatitude;
-        document.getElementById("geoLocationBox.southBoundLatitude").innerHTML = geoLocations.geoLocationBox.southBoundLatitude;
-        document.getElementById("geoLocationBox.westBoundLongitude").innerHTML = geoLocations.geoLocationBox.westBoundLongitude;
+        // ATTRIBUTES
+        // Data explaining the dataset
 
-        // MISC DATA
-        document.getElementById("Attributes.isActive").innerHTML = attributes.isActive;
-        document.getElementById("Attributes.state").innerHTML = attributes.state;
-        document.getElementById("Attributes.viewCount").innerHTML = attributes.viewCount;
-        document.getElementById("Attributes.downloadCount").innerHTML = attributes.downloadCount;
-        document.getElementById("Attributes.referenceCount").innerHTML = attributes.referenceCount;
-        document.getElementById("Attributes.citationCount").innerHTML = attributes.citationCount;
-        document.getElementById("Attributes.partCount").innerHTML = attributes.partCount;
-        document.getElementById("Attributes.partOfCount").innerHTML = attributes.partOfCount;
-        document.getElementById("Attributes.versionCount").innerHTML = attributes.versionCount;
-        document.getElementById("Attributes.versionOfCount").innerHTML = attributes.versionOfCount;
-        document.getElementById("Attributes.created").innerHTML = attributes.created;
-        document.getElementById("Attributes.registered").innerHTML = attributes.registered;
-        document.getElementById("Attributes.published").innerHTML = attributes.published;
-        document.getElementById("Attributes.updated").innerHTML = attributes.updated;
+        // Misc
+        addData("Attributes.isActive",attributes.isActive);
+        addData("Attributes.state",attributes.state);
+        addData("Attributes.viewCount",attributes.viewCount);
+        addData("Attributes.downloadCount",attributes.downloadCount);
+        addData("Attributes.referenceCount",attributes.referenceCount);
+        addData("Attributes.citationCount",attributes.citationCount);
+        addData("Attributes.partCount",attributes.partCount);
+        addData("Attributes.partOfCount",attributes.partOfCount);
+        addData("Attributes.versionCount",attributes.versionCount);
+        addData("Attributes.versionOfCount",attributes.versionOfCount);
+        
+        // Related Identifiers
+        // Also contains doi-sources which are duplicated in descriptions.doi
+        // They are placed here due to the doi-system tracking the use through this parameter
+        // But we instead fetch them from descriptions, as they there contain more data.
 
         let relatedIdentifiers = attributes.relatedIdentifiers;
         for (let i in relatedIdentifiers){
@@ -68,9 +92,13 @@ getDoiData = () => {
                 //console.log("add image time")
                 const image = document.createElement('img');
                 image.src  = item.relatedIdentifier;
-                document.getElementById('img.appender').appendChild(image);
+                appendData('img.appender',image);
             }
         }
+
+        // Descriptions
+        // Contains an abundance of descriptive data. 
+        // Looping and bundling by type to easier use relevant data only
 
         let desc = {};
         for(let i in attributes.descriptions){
@@ -84,9 +112,13 @@ getDoiData = () => {
                 desc[item.descriptionType] = [item.description];
             }
             
-        }
+        }        
+
         
-        console.log(desc);
+
+        // Descriptions.doi
+        // Contains all source datasets. Also those without a doi, but of doi-type data.
+        // If a doi is missing, it will instead contain an id.
 
         let doi = desc['DOI'];
        
@@ -96,26 +128,27 @@ getDoiData = () => {
             div.className = "listitem";
 
             let link = items[0];
-            let linkline = "";// There is an id here, but what type? what it do?
+            let linkline = ""; // There is an id here, but what type? what it do?
             
             if(link.includes("https")){
                 let doitext = link.replace("https://doi.org/","");
                 linkline = "<a href="+link+"/>"+doitext+"</a>";                
-            }
-            
+            }           
 
             let numberline = "<span> ("+ items[1]+" element)</span></br>";
             let nameline = "<span>"+ items[2]+"</span>";            
             div.innerHTML = nameline+numberline+linkline;
-            document.getElementById('doi.appender').appendChild(div);
+            appendData('doi.appender',div);
         }
 
         let artskartelement = desc['ArtskartUrl'][0];
         let a = document.createElement('div');
         a.className = "listitem";
         a.innerHTML = "<a href="+artskartelement+" >"+"Artskartlenke"+"</a>";
-        document.getElementById('a.appender').appendChild(a);
+        appendData('a.appender',a);
       
+
+        // End of all :)
         
     })
     .catch((err) => {
