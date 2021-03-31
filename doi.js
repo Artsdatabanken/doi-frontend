a1d074c300df09409ece632622fdad5696ffc4ba
Helene Engeness Mørk <helemork@gmail.com>
2021-03-31 11:38:12 +0200

    more robust, more beautiful

--- doi.js
+++ doi.js
@@ -23,6 +23,61 @@ function convertBytes(x){
     return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
 }
 
+function unWrap(wrapped,criteria,content){
+    if(wrapped == undefined || criteria == undefined || content == undefined) {
+        console.error("error in unwrap",criteria,content)
+        return null;
+    }
+    let unwrapped = {};
+    for(let i in wrapped){
+        let item = wrapped[i];                
+        let iwrap = item[criteria];
+        let exists = unwrapped[iwrap] || null;    
+        let newitem = item;
+        if(content !== false){
+            newitem = item[content];
+        }           
+
+        if(exists){
+            unwrapped[item[criteria]].push(newitem);                    
+        }else{
+            unwrapped[item[criteria]] = [newitem];
+        }                
+    }    
+    return unwrapped;   
+}
+
+function addData(id,content){
+    if(content!= undefined && id!= undefined){
+        try{
+            document.getElementById(id).innerHTML = content;}
+        catch{
+            console.error("failed for id: ",id,"and content:" ,content)
+        }
+    }else{
+        // To avoid entire page breaking down if one error occurs
+        try{
+            document.getElementById(id).innerHTML = "Mangler";}
+        catch{
+            console.error("failed for id: ",id,"and content:" ,content)
+        }
+    }            
+}
+
+function appendData(id,content){
+    if(content!= undefined && id!= undefined ){
+        try{
+            document.getElementById(id).appendChild(content);
+        } catch{
+            console.error("failed for id: ",id,"and content:" ,content)
+        }
+        
+    }else{
+        // To avoid entire page breaking down if one error occurs
+        console.error("no such content ", id,content);
+    }            
+}
+
 function getDoiData(){        
 
     // Obtaining the relevant doi to look up.
@@ -34,61 +89,19 @@ function getDoiData(){
     })
     .then((data) => {
 
-        function addData(id,content){
-            if(content!= undefined && id!= undefined){
-                document.getElementById(id).innerHTML = content;
-            }else{
-                // To avoid entire page breaking down if one error occurs
-                console.error("no such content ", id,content);
-            }            
-        }
-
-        function appendData(id,content){
-            if(content!= undefined && id!= undefined ){
-                document.getElementById(id).appendChild(content);
-            }else{
-                // To avoid entire page breaking down if one error occurs
-                console.error("no such content ", id,content);
-            }            
-        }
         
-        function unWrap(wrapped,criteria,content){
-            if(wrapped == undefined || criteria == undefined || content == undefined) {
-                console.error("error in unwrap",criteria,content)
-                return null;
-            }
-            let unwrapped = {};
-            for(let i in wrapped){
-                let item = wrapped[i];                
-                let iwrap = item[criteria];
-                let exists = unwrapped[iwrap] || null;    
-                let newitem = item;
-                if(content !== false){
-                    newitem = item[content];
-                }           
-    
-                if(exists){
-                    unwrapped[item[criteria]].push(newitem);                    
-                }else{
-                    unwrapped[item[criteria]] = [newitem];
-                }                
-            }    
-            return unwrapped;   
-        }
-
-
         // Work with JSON data here
-        addData("data.Id",data.data.id);
-        addData("data.Type",data.data.type);
+        //addData("data.Id",data.data.id);
+        //addData("data.Type",data.data.type);
 
         let attributes = data.data.attributes;
 
         // DOI URL 
         addData("Attributes.doi",attributes.doi);
         addData("Guid",getGuid());
-        addData("Attributes.prefix",attributes.prefix);
-        addData("Attributes.suffix",attributes.suffix);
-        addData("Attributes.identifiers",attributes.identifiers);
+        //addData("Attributes.prefix",attributes.prefix);
+        //addData("Attributes.suffix",attributes.suffix);
+        //addData("Attributes.identifiers",attributes.identifiers);
         addData("Creators.sourcename",attributes.creators[0].name);
         addData("Creators.sourcetype",attributes.creators[0].nameType);
 
@@ -116,7 +129,7 @@ function getDoiData(){
         // Data explaining the dataset
 
         // Misc
-        addData("Attributes.isActive",attributes.isActive);
+        //addData("Attributes.isActive",attributes.isActive);
         addData("Attributes.state",attributes.state);
         addData("Attributes.viewCount",attributes.viewCount);
         addData("Attributes.downloadCount",attributes.downloadCount);
@@ -126,6 +139,7 @@ function getDoiData(){
         addData("Attributes.partOfCount",attributes.partOfCount);
         addData("Attributes.versionCount",attributes.versionCount);
         addData("Attributes.versionOfCount",attributes.versionOfCount);
+
         
         // Related Identifiers
         // Also contains doi-sources which are duplicated in descriptions.doi
@@ -165,6 +179,7 @@ function getDoiData(){
         // Contains all source datasets. Also those without a doi, but of doi-type data.
         // If a doi is missing, it will instead contain an id.
 
+
         let doi = desc['DOI'];
        
         for (let i in doi){
@@ -185,6 +200,7 @@ function getDoiData(){
             div.innerHTML = nameline+numberline+linkline;
             appendData('doi.appender',div);
         }
+        console.log("is happy")
 
         // Artskart url:
 
@@ -194,16 +210,20 @@ function getDoiData(){
         a.innerHTML = "<a href="+artskartelement+" >"+"Artskartlenke"+"</a>";
         appendData('a.appender',a);
 
+        
+
         // Add areas if exists
         
-        let areas = desc['Areas'];    
-        if(areas.length >0)   { 
-        let ar = document.createElement('div');
-        ar.innerHTML = "<h3> Områder </h3>";
-        for (let i in areas){
-            ar.innerHTML += "<span>"+areas[i]+"</span>";
+        let areas = desc['Areas'];   
+        if(areas && areas.length >0)   { 
+            let ar = document.createElement('div');
+            ar.innerHTML = "<h3> Områder </h3>";
+                for (let i in areas){
+                    ar.innerHTML += "<span>"+areas[i]+"</span>";
+                }
+            appendData('area.appender',ar);
         }
-         appendData('area.appender',ar);}
+    
 
         // Misc data from descriptions
         addData("Descriptions.count",desc['Count']);
@@ -225,7 +245,7 @@ function getDoiData(){
         addData("Attributes.types.ris",attributes.types.ris);
 
         // Ymse attributes
-        addData("Attributes.version",attributes.version);
+        //addData("Attributes.version",attributes.version);
         addData("Attributes.rightsList",attributes.rightsList);
         addData("Attributes.url",attributes.url);
         addData("Attributes.metadataVersion",attributes.metadataVersion);
