e4ecbdd6e3e97ead18c00601d67bfcf59e074dc5
myrdrahl <even.juberg@gmail.com>
2021-04-06 07:45:45 +0200

    Transfering zipped dump.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 mkdir dump
 rsync -avr --exclude='*.sh' --exclude='*.yml' --exclude='/dump' . dump
+echo "Making archive"
+tar --directory=dump -zcf dump.tar.gz .
+sshpass -p $scp_pass scp -v -o StrictHostKeyChecking=no dump.tar.gz $scp_user@$scp_dest/
 curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy doi-frontend"}' $slackaddy
