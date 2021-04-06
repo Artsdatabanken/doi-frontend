67ac289ac30dea851edb0e8eed95b6d44db39510
myrdrahl <even.juberg@gmail.com>
2021-04-06 07:58:38 +0200

    Installing sshpass in virtual build env.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ mkdir dump
 rsync -avr --exclude='*.sh' --exclude='*.yml' --exclude='/dump' . dump
 echo "Making archive"
 tar --directory=dump -zcf dump.tar.gz .
+ls -l
 sshpass -p $scp_pass scp -v -o StrictHostKeyChecking=no dump.tar.gz $scp_user@$scp_dest/
 curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy doi-frontend"}' $slackaddy
