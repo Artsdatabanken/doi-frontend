4f6dccef70fad87e42ad98e71ebdd4bde5da0d5c
Helene Engeness Mørk <helemork@gmail.com>
2021-04-19 11:56:49 +0200

    menu better

--- header.js
+++ header.js
@@ -14,18 +14,21 @@ function getHeaderMenu(){
                 for (let i in apimenus){
                     let item = apimenus[i];
                     let id = item.Values.toString().replace(" ","");
-
                     // Using createelement to enable attachment of eventlistener
                     let menubutton = document.createElement('button');
                     menubutton.className = "menuitems";
-
+                    let buttonname = item.Values;
+                    let dropicon = " <span class='material-icons'>arrow_drop_down</span>";
+                    if(buttonname == "English"){
+                        // TODO : WHEN DECIDED HOW WE WANT IT: MAKE THE ENGLISH BUTTON
+                        //menubutton.className += " englishbutton";
+                        //menubutton.innerHTML = "<img src='https://www.artsdatabanken.no/Content/Images/english.svg'></img>"
+                    }
                     // Generate the dropdowncontent 
-                    let newdropdown ="<ul class='dropdown' id='"+id+"' style='display:none'>";                
-                    let subitems = item.References;
-                    
+                    let newdropdown ="<ul class='dropdown' id='"+id+"' style='display:none'>";      
+                    let subitems = item.References;                    
                     for(let j in subitems){
-                        subitem = subitems[j];
-                        
+                        subitem = subitems[j];                        
                         let infotext = "";
                         let link = null;
                         for(let k in subitem.Records){
@@ -35,12 +38,10 @@ function getHeaderMenu(){
                             infotext = "<span>"+infotext+"</span>";
                            }else{
                             link = subsub.Values.toString();
-                           }
-                           
+                           }                           
                         }
                         let name = subitem.Heading;
-                        let external = "";
-                        
+                        let external = "";                        
                         let url = "https://artsdatabanken.no"+subitem.Url;
                         if(!subitem.Heading){
                             name= subitem.Header.Content;                           
@@ -50,25 +51,32 @@ function getHeaderMenu(){
                             external = " <b class='material-icons'>open_in_new</b>";
                             url = link;
                         }
-
-                        
-                        
                         newdropdown += "<li><a href='"+url+"'>"+name+external+infotext+"</a></li>";
-
                     }                
                     newdropdown +="</ul>";
-                    menubutton.innerHTML = item.Values+newdropdown; // attach it
+                    
+                    // ADD ALL BUTTONCNTENT
+                    menubutton.innerHTML = buttonname+dropicon+ newdropdown; // attach it
 
                     // Toggle the relevant dropdownmenu
                     menubutton.addEventListener('click',function(e){
                         let target = e.target.querySelector('.dropdown');
+                        let siblings = $(".dropdown");
+                        let show = false;
                         if(target.style.display == "none"){
+                            show = true;
+                        }
+                        for(let i in siblings){
+                            // hide all the siblings before dealing with the current
+                            let sibling = siblings[i];
+                            if(sibling.style){
+                                sibling.style.display = "none";
+                            }                            
+                        }
+                        if(show){
                             target.style.display = "block";
-                        }else{
-                            target.style.display = "none";
                         }
                     });
-
                     // Add to page
                     appendData('headermenu',menubutton);
                 }
